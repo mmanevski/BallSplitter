cfb535758cba5e21acac519a6679fd46efa5c958
mmanevski87 <48494289+mmanevski87@users.noreply.github.com>
2020-06-06 14:37:34 +0200

    balls out of bounds handling

---
+++ Assets/Scripts/Gameplay/BallDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameData;
+using UnityEngine;
+
+public class BallDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(ObjectTags.ballTag))
+        {
+            other.GetComponent<BallController>().Despawn();
+
+        }
+    }
+}
